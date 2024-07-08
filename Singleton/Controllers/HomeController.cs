7975f5889468267f19ced339854bb4a142f07227
mujeebfive <mujeebfive@gmail.com>
2024-07-08 11:29:01 +0530

    Add project files.

---
+++ Singleton/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Singleton.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var obj = Singleton.GetInstance(1);
+            var obj2 = Singleton.GetInstance(2);
+
+            ViewBag.Singleton = "instance1 ="+ obj.id+"  "+ "instance2 =" + obj2.id; 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        public int id { get;private set; }
+        private Singleton()
+        {
+        }
+        private static readonly object padlock = new object();
+        public static Singleton GetInstance(int id)
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                        instance.id = id;
+                    }
+                    return instance;
+                }
+
+            }
+            return instance;
+
+        }
+        public void Method()
+        {
+            //logic..
+        }
+
+
+
+    }
+}
